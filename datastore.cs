fc27c3059dbd908ebf235be4ac82897d00428f31
Daniel Werthén <daniel.werthen@netlight.com>
2014-05-07 14:19:38 +0200

    Adding a structure and implement cool stuff

---
+++ datastore.cs
@@ -0,0 +1,3 @@
+Load data
+
+Reprehenderit dolore consequat mollit laborum ea laborum do Lorem elit duis in incididunt. Pariatur adipisicing laborum consequat aliqua. Non exercitation ullamco tempor laboris sit tempor et incididunt. Tempor velit est do do est reprehenderit ex. Anim consectetur eiusmod aliquip veniam aute nostrud.
